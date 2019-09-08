ec70991980eb42314df78730f347fca362170af9
rupeshjadhav <jadhavrupesh22@gmail.com>
2019-09-08 19:19:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exomusicplayer/MainActivity.java
@@ -0,0 +1,158 @@
+package com.example.exomusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerControlView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Assertions;
+
+public class MainActivity extends AppCompatActivity implements ExoPlayer.EventListener {
+
+    private Handler mainHandler;
+    private RenderersFactory renderersFactory;
+    private BandwidthMeter bandwidthMeter;
+    private LoadControl loadControl;
+    private DataSource.Factory dataSourceFactory;
+    private ExtractorsFactory extractorsFactory;
+    private MediaSource mediaSource;
+    private TrackSelection.Factory trackSelectionFactory;
+    private SimpleExoPlayer player;
+    private final String streamUrl = "http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-einws"; //bbc world service url
+    private TrackSelector trackSelector;
+    PlayerControlView playerControlView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playerControlView=findViewById(R.id.exo_controller);
+        PlayMusic();
+
+
+
+    }
+
+    private void PlayMusic() {
+
+        renderersFactory = new DefaultRenderersFactory(getApplicationContext());
+        bandwidthMeter = new DefaultBandwidthMeter();
+        trackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        trackSelector = new DefaultTrackSelector(trackSelectionFactory);
+        loadControl = new DefaultLoadControl();
+        player = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector, loadControl);
+        player.addListener(this);
+
+        dataSourceFactory = new DefaultDataSourceFactory(getApplicationContext(), "ExoplayerDemo");
+        extractorsFactory = new DefaultExtractorsFactory();
+        mainHandler = new Handler();
+        mediaSource = new ExtractorMediaSource(Uri.parse(streamUrl),
+                dataSourceFactory,
+                extractorsFactory,
+                mainHandler,
+                null);
+        playerControlView.setPlayer(player);
+        player.setPlayWhenReady(false);
+        player.prepare(mediaSource);
+       playerControlView.setShowTimeoutMs(0);
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        player.setPlayWhenReady(true);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.setPlayWhenReady(false);
+
+    }
+
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
+    }
+}
